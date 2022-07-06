d082ad9b4441b3af5b924e2e52230701c2205db8
MasterM <christianotteromarques@gmail.com>
2022-07-06 18:20:17 -0300

    primeir console C#

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace PrimeiroConsole;
+
+ class Program{
+
+ static void Main(string[] args)
+ {
+
+
+int numeroDeVezes = 5;
+
+for (int i = 0; i < numeroDeVezes; i++){
+
+Console.WriteLine($"Primeiro console versão {i}");
+
+}
+}
+
+ 
+ }
